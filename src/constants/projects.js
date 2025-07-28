2fd84e951221b410f0bef3b539630b64a4ee8b36
tlapanco <99222342+tlapanco@users.noreply.github.com>
2025-07-28 17:18:41 -0600

    Fix - Projects descriptions

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -11,39 +11,39 @@ export const PROJECTS = [
 	{
 		img: project1,
 		title: "CRUD - UPMP Departamento vinculación",
-		description: "Aplicación para la administración de los procesos de estancia/estadía de los alumnos",
+		description: "Aplicación para la administración de los procesos de estancia/estadía de los alumnos (Laravel, JQuery, Bootstrap, MySQL)",
 		links: {
 			site: "https://demo-vinculacion.infinityfreeapp.com/proyecto_vinculacion/public/auth/login?i=1",
 			github: "https://github.com/tlapanco/crud_laravel",
 		},
-	},
+	},	
 	{
-		img: project2,
-		title: "PDF Merger - Laravel",
-		description: "Aplicación para la unión de archivos PDF",
+		img: project8,
+		title: "Meow Boutique",
+		description: "Carrito de compras, diseño responsivo y pedidos por mensaje de What's app con el listado de productos (React js, Tailwind CSS)",
 		links: {
-			site: "https://tl-developer-site.infinityfreeapp.com/merge2pdf/public/",
-			github: "https://github.com/tlapanco/laravel-merge2pdf",
+			site: "https://tlapanco.github.io/m-store",
+			github: "https://github.com/tlapanco/react-store",
 		},
 	},
 	{
 		img: project3,
-		title: "GUI - YouTube downloader",
-		description: "Aplicación para la descarga de videos de YouTube",
+		title: "YouTube downloader - GUI",
+		description: "Aplicación para la descarga de videos de YouTube (Python, Tkinter)",
 		links: {
 			site: "https://drive.google.com/file/d/19aeGqWgcFIgu97yKs8-7V4d5ZvBP83DG/view?usp=drivesdk",
 			github: "https://github.com/tlapanco/gui-yt-downloader",
 		},
 	},
 	{
-		img: project8,
-		title: "Meow Boutique",
-		description: "Carrito de compras para una tienda ropa hecha en React",
+		img: project2,
+		title: "PDF Merger",
+		description: "Aplicación para la unión de archivos PDF (Laravel, Tailwind CSS, Livewire)",
 		links: {
-			site: "https://tlapanco.github.io/m-store",
-			github: "https://github.com/tlapanco/react-store",
+			site: "https://tl-developer-site.infinityfreeapp.com/merge2pdf/public/",
+			github: "https://github.com/tlapanco/laravel-merge2pdf",
 		},
-	},
+	},		
 	{
 		img: project4,
 		title: "NIKE - Landing page",
@@ -56,7 +56,7 @@ export const PROJECTS = [
 	{
 		img: project5,
 		title: "TIC TAC TOE - Game",
-		description: "Landing page de nike para la práctica de maquetación con el framework de Tailwind CSS",
+		description: "Juego de tres en raya para la práctica de estados (React js)",
 		links: {
 			site: "https://tlapanco.github.io/juego-gato-react",
 			github: "https://github.com/tlapanco/juego-gato-react",
@@ -65,7 +65,7 @@ export const PROJECTS = [
 	{
 		img: project6,
 		title: "Pockedex",
-		description: "Landing page de nike para la práctica de maquetación con el framework de Tailwind CSS",
+		description: "Pequeña pockedex para la práctica de vanilla js (JavaScript, CSS)",
 		links: {
 			site: "https://tlapanco.github.io/JS-frontend-mission",
 			github: "https://github.com/tlapanco/juego-gato-react",
@@ -74,7 +74,7 @@ export const PROJECTS = [
 	{
 		img: project7,
 		title: "Next marvel film",
-		description: "Página web sencilla hecha con PHP puro para la práctica de clases y la creación de pequeños motores de plantilla con php consultando una API",
+		description: "Página web para la práctica de clases y la creación de pequeños motores de plantillas con php consultando una API (PHP)",
 		links: {
 			site: "https://tl-developer-site.infinityfreeapp.com/next-marvel-film",
 			github: "https://github.com/tlapanco/php-simple-api",
